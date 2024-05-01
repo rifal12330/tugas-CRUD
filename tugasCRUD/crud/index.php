ee58f60e7826a0d66530e5b9b8088862e324bac2
Muhammad Rifal Rifdiansyah <119149644+rifal12330@users.noreply.github.com>
2024-05-01 22:17:18 +0700

    Update index.php

--- tugasCRUD/crud/index.php
+++ tugasCRUD/crud/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sekolah Sepak Bola</title>
+    <title>daftar mahasiswa fasilkom</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <style>
         /* Custom CSS untuk tampilan yang lebih menarik */
